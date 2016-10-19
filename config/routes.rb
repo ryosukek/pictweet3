1b5e51109ff6548ae475caff8ddd8dac2f615eda
Ryosuke Kawauchi <ryosukekawauchi@kawauchiryouyuu-no-MacBook-Air.local>
2016-10-19 18:55:42 +0900

    削除機能完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'tweets#index'                   # ルートパスの設定
-  get 'tweets'     => 'tweets#index'    # ツイート一覧画面
-  get 'tweets/new' => 'tweets#new'      # ツイート投稿画面
-  post 'tweets'    => 'tweets#create'   # ツイート投稿機能
-  get 'users/:id'  => 'users#show'      # Mypageへのルーティング
+  root 'tweets#index'                      # ルートパスの設定
+  get 'tweets'        => 'tweets#index'    # ツイート一覧画面
+  get 'tweets/new'    => 'tweets#new'      # ツイート投稿画面
+  post 'tweets'       => 'tweets#create'   # ツイート投稿機能
+  delete 'tweets/:id' => 'tweets#destroy'  #ツイートの削除
+  get 'users/:id'     => 'users#show'      # Mypageへのルーティング
 end
